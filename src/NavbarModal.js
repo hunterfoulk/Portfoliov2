6a73e8cff96478e166b5a453524a0efec3946f7b
hunter foulk <huntertehjakey@hotmail.com>
2020-07-08 11:35:04 -0700

    dev ops

--- src/NavbarModal.js
+++ src/NavbarModal.js
@@ -11,11 +11,10 @@ export default function NavbarModal({ setModal, setDropdown, emailClose }) {
 
   const sendEmail = async (e) => {
     e.preventDefault();
-    // https://elegant-haibt-a61338.netlify.app/.netlify/functions/server/portfolio/sendemail
-    await axios
 
+    await axios
       .post(
-        "http://localhost:9000/.netlify/functions/server/portfolio/sendemail",
+        "https://elegant-haibt-a61338.netlify.app/.netlify/functions/server/portfolio/sendemail",
         {
           name: name,
           email: email,
