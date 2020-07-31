640962e0a2b341a22f5e9441d3f279e29a16a5ac
hunter foulk <huntertehjakey@hotmail.com>
2020-07-31 09:06:24 -0700

    name

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./contact.scss";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Contact() {
   const [name, setName] = useState("");
@@ -34,6 +36,7 @@ export default function Contact() {
         console.log("server response", res);
         console.log("email has been sent");
         setSuccess(true);
+        notify();
       })
       .catch((error) => console.error("email was not succesfull:", error));
 
@@ -42,16 +45,67 @@ export default function Contact() {
     setMessage("");
   };
 
+  const notify = () =>
+    toast.success("Your email has been sent! thank you! ", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
   return (
     <div className="contact-main">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div className="form-container">
         <div className="contact-header">
           <h1>Contact Me</h1>
         </div>
-        <form>
-          <input placeholder="Name..." type="text" />
-          <input placeholder="Email..." type="text" />
-          <textarea placeholder="Message..." rows="4" cols="50" type="text" />
+        <form
+          onSubmit={(e) => {
+            sendEmail(e);
+          }}
+        >
+          <input
+            placeholder="Name..."
+            type="text"
+            onChange={(e) => {
+              setName(e.target.value);
+              console.log(e.target.value);
+            }}
+          />
+          <input
+            placeholder="Email..."
+            type="text"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              console.log(e.target.value);
+            }}
+          />
+          <textarea
+            placeholder="Message..."
+            rows="4"
+            cols="50"
+            type="text"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+              console.log(e.target.value);
+            }}
+          />
           <button type="submit">Submit</button>
         </form>
       </div>
